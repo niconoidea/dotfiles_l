1fe0e31af13f1e4689d812239af0d38b87088c74
niconoidea <nicolasvonwirth@icloud.com>
2025-12-02 12:51:16 -0300

    dotfile update

--- waybar/scripts/brightness.sh
+++ waybar/scripts/brightness.sh
@@ -3,8 +3,8 @@ brightness=$(brightnessctl g)
 max_brightness=$(brightnessctl m)
 percent=$((brightness * 100 / max_brightness))
 
-if [ "$percent" -lt 3 ]; then
-    percent=3
+if [ "$percent" -lt 5 ]; then
+    percent=5
 fi
 
 echo " $percent%"
